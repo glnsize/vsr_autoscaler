3a3e9be2a8821868dc07ea1c365d0bb08c9f1321
Glenn Sizemore <twitter.com/glnsize>
2019-08-07 16:54:05 -0400

    SDDC Scale up logic added

--- vsr_watcher.py
+++ vsr_watcher.py
@@ -21,6 +21,8 @@ import requests
 import socket
 import ipaddress
 
+from com.vmware.vmc.model_client import EsxConfig, ErrorResponse
+from com.vmware.vapi.std.errors_client import InvalidRequest
 from vmware.vapi.vmc.client import create_vmc_client
 
 class vsr_watcher(object):
@@ -33,6 +35,7 @@ class vsr_watcher(object):
     def __init__(self):
         self.dr_network = None
         self.sddc_id = None
+        self.sddc = None
         self.org_id = None
         self.cluster_name = None
         self.cluster_size = None
@@ -101,6 +104,7 @@ class vsr_watcher(object):
             print("we in the cloud")
             return True
         else: 
+            print(host_ip)
             print('we on the ground')
             return False
 
@@ -120,32 +124,55 @@ class vsr_watcher(object):
         if self.sddc_id not in [sddc.id for sddc in sddcs]:
             raise ValueError("SDDC with ID {} doesn't exist in org {}".
                              format(self.sddc_id, self.org_id))
-        sddc = self.vmc_client.org.sddc.get(self.org_id,self.sddc_id)
+        self.sddc = self.vmc_client.orgs.Sddcs.get(self.org_id,self.sddc_id)
 
         # Check if the cluster exists
-        if self.cluster_name not in [cluster for cluster in sddc.resource_config.clusters]:
+        if self.cluster_name not in [cluster.cluster_name for cluster in self.sddc.resource_config.clusters]:
             raise ValueError("Specified Cluster {} doesn't exist in SDDC {}".
-                             format(self.cluster_name, self.sddc_id))
-                             
-    def checkSDDC(self):
-        sddc = self.vmc_client.org.sddc.get(self.org_id,self.sddc_id)
-        # get the current Cluster size
-        for cluster in sddc.resource_config.clusters:
+                             format(self.cluster_name, self.sddc.name))
+    
+    def fix_cluster_size(self,host_count):
+        if self.sddc.sddc_state != self.sddc.SDDC_STATE_READY:
+            raise ValueError("Specified Cluster {} in SDDC {} not ready current status {}".
+                             format(self.cluster_name, self.sddc.name,self.sddc.sddc_state))
+        print('SCALE UP {} in {} SDDC by adding {} hosts'.
+            format(self.cluster_name,self.sddc.name,host_count))
+        esx_config = EsxConfig(num_hosts=host_count)
+        try:
+            task = self.vmc_client.orgs.sddcs.Esxs.create(org=self.org_id,
+                                                          sddc=self.sddc_id,
+                                                          esx_config=esx_config)
+        except InvalidRequest as e:
+            # Convert InvalidRequest to ErrorResponse to get error message
+            error_response = e.data.convert_to(ErrorResponse)
+            raise Exception(error_response.error_messages)
+
+        print('Host Add initiated, Task ID: {} Status: {}'.format(task.id,task.status))
+        
+    
+    def get_cluster_size(self):
+        for cluster in self.sddc.resource_config.clusters:
             if (cluster.cluster_name == self.cluster_name):
-                sddc_cluster_size = len(cluster.esx_host_list)
-        # check size
-        if self.cluster_size > sddc_cluster_size:
-            num_to_add = self.cluster_size - sddc_cluster_size
-            print('SCALE UP cluster {} in SDDC {} by adding {} hosts'.
-                  format(self.cluster_name,sddc.name,num_to_add))
+                return len(cluster.esx_host_list)
+    
+    def check_cluster_size(self,current_cluster_size):
+        if self.cluster_size > current_cluster_size:
+            return self.cluster_size - current_cluster_size
+            
+
 
 def main():
     monitor = vsr_watcher()
     monitor.options()
     if monitor.check_running_in_dr_network():
         monitor.setup()
-        monitor.checkSDDC()
-
+        host_count = monitor.check_cluster_size(monitor.get_cluster_size())
+        if host_count >= 1:
+            monitor.fix_cluster_size(host_count)
+        elif host_count >= -1:
+            print('we too big scale down...')
+            print('... relevant comment found: TODO: Implement scale down')
+        
 if __name__ == '__main__':
     main()
     
